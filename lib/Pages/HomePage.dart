d1c1d237d960db1e89c6ed4d0c77c888330e038c
Tarık KAYA <tiqqq24@gmail.com>
2020-07-17 16:49:46 +0300

    new category and icons added

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -19,27 +19,38 @@ class _HomePageState extends State<HomePage> {
   AudioCache audioCache = new AudioCache();
   List<SoundojiObj> funList;
   List<SoundojiObj> animalList;
+  List<SoundojiObj> vehiclesList;
 
   List<SoundojiObj> soundojis = [
-    SoundojiObj(category : 'Fun' ,soundPath: 'sounds/slap.wav', iconPath: 'assets/images/slap.svg'),
-    SoundojiObj(category : 'Fun' ,soundPath: 'sounds/pew.wav', iconPath: 'assets/images/pew.svg'),
-    SoundojiObj(category : 'Fun' ,soundPath: 'sounds/mp5.wav', iconPath: 'assets/images/mp5.svg'),
-    SoundojiObj(category : 'Fun' ,soundPath: 'sounds/arrow.mp3', iconPath: 'assets/images/arrow.svg'),
-    SoundojiObj(category : 'Fun' ,soundPath: 'sounds/kid.mp3', iconPath: 'assets/images/baby.svg'),
-    SoundojiObj(category : 'Animal' ,soundPath: 'sounds/cow.mp3', iconPath: 'assets/images/cow.svg'),
-    SoundojiObj(category : 'Animal' ,soundPath: 'sounds/dog.mp3', iconPath: 'assets/images/dog.svg'),
-    SoundojiObj(category : 'Animal' ,soundPath: 'sounds/sheep.mp3', iconPath: 'assets/images/sheep.svg'),
-    SoundojiObj(category : 'Animal' ,soundPath: 'sounds/horse.mp3', iconPath: 'assets/images/horse.svg'),
-    SoundojiObj(category : 'Animal' ,soundPath: 'sounds/bird.mp3', iconPath: 'assets/images/bird.svg'),
-    SoundojiObj(category : 'Animal' ,soundPath: 'sounds/lion.mp3', iconPath: 'assets/images/lion.svg'),
+    SoundojiObj(category : 'Funny' ,soundPath: 'sounds/slap.wav', iconPath: 'assets/images/slap.svg'),
+    SoundojiObj(category : 'Funny' ,soundPath: 'sounds/pew.wav', iconPath: 'assets/images/pew.svg'),
+    SoundojiObj(category : 'Funny' ,soundPath: 'sounds/flop.mp3', iconPath: 'assets/images/flop.svg'),
+    SoundojiObj(category : 'Funny' ,soundPath: 'sounds/mp5.wav', iconPath: 'assets/images/mp5.svg'),
+    SoundojiObj(category : 'Funny' ,soundPath: 'sounds/water.mp3', iconPath: 'assets/images/water.svg'),
+    SoundojiObj(category : 'Funny' ,soundPath: 'sounds/arrow.mp3', iconPath: 'assets/images/arrow.svg'),
+    SoundojiObj(category : 'Funny' ,soundPath: 'sounds/kid.mp3', iconPath: 'assets/images/baby.svg'),
+    SoundojiObj(category : 'Funny' ,soundPath: 'sounds/kiss.mp3', iconPath: 'assets/images/kiss.svg'),
+
+    SoundojiObj(category : 'Animals' ,soundPath: 'sounds/cow.mp3', iconPath: 'assets/images/cow.svg'),
+    SoundojiObj(category : 'Animals' ,soundPath: 'sounds/dog.mp3', iconPath: 'assets/images/dog.svg'),
+    SoundojiObj(category : 'Animals' ,soundPath: 'sounds/cat.mp3', iconPath: 'assets/images/cat.svg'),
+    SoundojiObj(category : 'Animals' ,soundPath: 'sounds/sheep.mp3', iconPath: 'assets/images/sheep.svg'),
+    SoundojiObj(category : 'Animals' ,soundPath: 'sounds/horse.mp3', iconPath: 'assets/images/horse.svg'),
+    SoundojiObj(category : 'Animals' ,soundPath: 'sounds/bird.mp3', iconPath: 'assets/images/bird.svg'),
+    SoundojiObj(category : 'Animals' ,soundPath: 'sounds/lion.mp3', iconPath: 'assets/images/lion.svg'),
+
+    SoundojiObj(category: 'Vehicles', soundPath: 'sounds/car.mp3', iconPath:'assets/images/car.svg'),
+
   ];
 
   play(int index, String category) async {
     switch (category){
-      case 'Fun' :
+      case 'Funny' :
         audioCache.play(funList[index].soundPath); break;
-      case 'Animal':
+      case 'Animals':
         audioCache.play(animalList[index].soundPath); break;
+      case 'Vehicles':
+        audioCache.play(vehiclesList[index].soundPath); break;
     }
   }
 
@@ -49,8 +60,9 @@ class _HomePageState extends State<HomePage> {
 
   @override
   void initState() {
-    funList = filterByCategory('Fun');
-    animalList = filterByCategory('Animal');
+    funList = filterByCategory('Funny');
+    animalList = filterByCategory('Animals');
+    vehiclesList = filterByCategory('Vehicles');
     super.initState();
   }
 
@@ -62,12 +74,9 @@ class _HomePageState extends State<HomePage> {
       padding: const EdgeInsets.symmetric(horizontal : 12.0),
       child: ListView(
         children: [
-          CategoryList(title: 'Fun',list: funList,playSound:play, ),
-          CategoryList(title: 'Animal',list: animalList,playSound:play, ),
-          CategoryList(title: 'Fun',list: funList,playSound:play, ),
-          CategoryList(title: 'Animal',list: animalList,playSound:play, ),
-          CategoryList(title: 'Fun',list: funList,playSound:play, ),
-          CategoryList(title: 'Animal',list: animalList,playSound:play, ),
+          CategoryList(title: 'Funny',list: funList,playSound:play, ),
+          CategoryList(title: 'Animals',list: animalList,playSound:play, ),
+          CategoryList(title: 'Vehicles',list: vehiclesList,playSound:play, ),
         ],
       ),
     );
